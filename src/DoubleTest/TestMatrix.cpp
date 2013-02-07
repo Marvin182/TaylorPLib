5e261ecb79022d518313bc4b51059a37ae73c6e9
Marvin Ritter <marvin.ritter@gmail.com>
2013-02-07 19:37:31 +0100

    rewrote Matrix Test to work with Polynomials instead of doubles, which actually just shows the current memory allocation problem in the Matrix class

--- src/DoubleTest/TestMatrix.cpp
+++ src/DoubleTest/TestMatrix.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 using namespace LibMatrix;
 
+#define T(n) Polynomial() * (n)
+
 namespace LibMatrix {
 	// needed by GTest to print matrices if some ASSERT_EQ(matrix1, matrix2) failed
 	std::ostream& operator<<(std::ostream &out, const Matrix &m)
@@ -20,6 +22,11 @@ namespace LibMatrix {
 		}
 		return out; 
 	}
+
+	bool operator==(double x, const Polynomial &p)
+	{
+		return p.isConst() && p[0] == x;
+	}
 }
 
 /*
@@ -33,58 +40,58 @@ class MatrixOperator: public ::testing::Test
 		MatrixOperator()
 		{
 			// A
-			double a[] = {
-				1, 2,
-				3, 4
+			Polynomial a[] = {
+				T(1), T(2),
+				T(3), T(4)
 			};
 			A = Matrix(2, 2, a);
 
 			// B
-			double b[] = {
-				5, 2,
-				0, 3
+			Polynomial b[] = {
+				T(5), T(2),
+				T(0), T(3)
 			};
 			B = Matrix(2, 2, b);
 
 			// A + B
-			double aplusb[] = {
-				6, 4,
-				3, 7 
+			Polynomial aplusb[] = {
+				T(6), T(4),
+				T(3), T(7) 
 			};
 			AplusB = Matrix(2, 2, aplusb);
 
 			// - A
-			double minusa[] = {
-				-1, -2,
-				-3, -4
+			Polynomial minusa[] = {
+				T(-1), T(-2),
+				T(-3), T(-4)
 			};
 			minusA = Matrix(2, 2, minusa);
 
 			// A - B
-			double aminusb[] = {
-				-4, 0,
-				 3, 1
+			Polynomial aminusb[] = {
+				T(-4), T(0),
+				 T(3), T(1)
 			};
 			AminusB = Matrix(2, 2, aminusb);
 
-			// 2 * A
-			double twoa[] = {
-				2, 4,
-				6, 8
+			// T(2) * A
+			Polynomial twoa[] = {
+				T(2), T(4),
+				T(6), T(8)
 			};
 			twoA = Matrix(2, 2, twoa);
 
 			// A * B
-			double ab[] = {
-				 5,  8,
-				15, 18
+			Polynomial ab[] = {
+				 T(5),  T(8),
+				T(15), T(18)
 			};
 			AB = Matrix(2, 2, ab);			
 			
 			// B * A
-			double ba[] = {
-				11, 18,
-				 9, 12
+			Polynomial ba[] = {
+				T(11), T(18),
+				 T(9), T(12)
 			};
 			BA = Matrix(2, 2, ba);
 		}
@@ -96,7 +103,7 @@ class MatrixMultiplication: public ::testing::Test
 		Matrix A, B, C, I;
 		double alpha, beta;
 
-		double getRandomNumber() { return rand() % 10; }
+		Polynomial getRandomNumber() { return T(rand() % 10); }
 		void fillWithRandoms(Matrix &m)
 		{
 			for (int i = 0; i < m.nrows(); i++)
@@ -133,79 +140,79 @@ class MatrixMethods: public ::testing::Test
 
 		MatrixMethods()
 		{
-			double i[] = {
-				1, 0, 0,
-				0, 1, 0,
-				0, 0, 1
+			Polynomial i[] = {
+				T(1), T(0), T(0),
+				T(0), T(1), T(0),
+				T(0), T(0), T(1)
 			};
 			I = Matrix(3, 3, i);
 
-			double z[] = {
-				0, 0, 0,
-				0, 0, 0,
-				0, 0, 0
+			Polynomial z[] = {
+				T(0), T(0), T(0),
+				T(0), T(0), T(0),
+				T(0), T(0), T(0)
 			};
 			Z = Matrix(3, 3, z);
 
-			double a[] = {
-				1, 2, 3,
-				4, 5, 6,
-				7, 8, 9
+			Polynomial a[] = {
+				T(1), T(2), T(3),
+				T(4), T(5), T(6),
+				T(7), T(8), T(9)
 			};
 			A = Matrix(3, 3, a);
 
-			double b[] = {
-				1,  2,  3,  4,
-				5,  6,  7,  8,
-				9, 10, 11, 12
+			Polynomial b[] = {
+				T(1),  T(2),  T(3),  T(4),
+				T(5),  T(6),  T(7),  T(8),
+				T(9), T(10), T(11), T(12)
 			};
 			B = Matrix(3, 4, b);
 
-			double c[] = {
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5
+			Polynomial c[] = {
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5)
 			};
 			C = Matrix(8, 7, c);
 
-			double cz[] = {
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 0, 0, 5, 5, 5,
-				5, 5, 0, 0, 5, 5, 5,
-				5, 5, 0, 0, 5, 5, 5,
-				5, 5, 0, 0, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5
+			Polynomial cz[] = {
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(0), T(0), T(5), T(5), T(5),
+				T(5), T(5), T(0), T(0), T(5), T(5), T(5),
+				T(5), T(5), T(0), T(0), T(5), T(5), T(5),
+				T(5), T(5), T(0), T(0), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5)
 			};
 			CZ = Matrix(8, 7, cz);
 
-			double cid[] = {
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 1, 0, 0, 0, 5,
-				5, 5, 0, 1, 0, 0, 5,
-				5, 5, 0, 0, 1, 0, 5,
-				5, 5, 0, 0, 0, 1, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5
+			Polynomial cid[] = {
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(1), T(0), T(0), T(0), T(5),
+				T(5), T(5), T(0), T(1), T(0), T(0), T(5),
+				T(5), T(5), T(0), T(0), T(1), T(0), T(5),
+				T(5), T(5), T(0), T(0), T(0), T(1), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5)
 			};
 			CId = Matrix(8, 7, cid);
 
-			double c3[] = {
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 3, 3, 3, 3, 5, 5,
-				5, 3, 3, 3, 3, 5, 5,
-				5, 3, 3, 3, 3, 5, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5,
-				5, 5, 5, 5, 5, 5, 5
+			Polynomial c3[] = {
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(3), T(3), T(3), T(3), T(5), T(5),
+				T(5), T(3), T(3), T(3), T(3), T(5), T(5),
+				T(5), T(3), T(3), T(3), T(3), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5),
+				T(5), T(5), T(5), T(5), T(5), T(5), T(5)
 			};
 			C3 = Matrix(8, 7, c3);
 		}
@@ -218,19 +225,19 @@ protected:
 
 	MatrixExceptions()
 	{
-		double a[] = {
-			1, 6, 0,
-			2, 7, 0,
-			3, 8, 0
+		Polynomial a[] = {
+			T(1), T(6), T(0),
+			T(2), T(7), T(0),
+			T(3), T(8), T(0)
 		};
-		double b[] = {
-			1, 6,
-			2, 7,
-			3, 8
+		Polynomial b[] = {
+			T(1), T(6),
+			T(2), T(7),
+			T(3), T(8)
 		};
 
-		A = Matrix(3,3, a);
-		B = Matrix(3,2, b);
+		A = Matrix(3, 3, a);
+		B = Matrix(3, 2, b);
 	}
 };
 
@@ -239,11 +246,11 @@ protected:
  */
 TEST(MatrixConstructor, default_constructor)
 {
-	// default constructor should create a zero initialized 1x1 matrix 
+	// default constructor should create a zero initialized T(1)x1 matrix 
 	Matrix d;
 	ASSERT_EQ(1, d.nrows());
 	ASSERT_EQ(1, d.ncols());
-	ASSERT_EQ(0, d(0, 0));
+	ASSERT_EQ(T(0), d(0, 0));
 }
 
 TEST(MatrixConstructor, regular_constructor)
@@ -259,10 +266,10 @@ TEST(MatrixConstructor, regular_constructor)
 TEST(MatrixConstructor, test_and_copy_constructor)
 {
 	// test constructor
-	double values[] = {
-		1, 2,	// first row
-		3, 4,	// second row
-		5, 6	// third row
+	Polynomial values[] = {
+		T(1), T(2),	// first row
+		T(3), T(4),	// second row
+		T(5), T(6)	// third row
 	};
 	Matrix t(3, 2, values); // rows, columns, values
 	// t.print("t");
@@ -278,8 +285,8 @@ TEST(MatrixConstructor, test_and_copy_constructor)
 	// copy constructor
 	Matrix c(t);
 	// c should be a deep copy of t
-	t(0, 1) = 8;
-	t(2, 0) = 9;
+	t(0, 1) = T(8);
+	t(2, 0) = T(9);
 	ASSERT_EQ(8, t(0, 1));
 	ASSERT_EQ(9, t(2, 0));
 
@@ -384,12 +391,12 @@ TEST_F(MatrixMultiplication, mmCaABbC)
 TEST_F(MatrixMultiplication, bmmCaABbC)
 {
    	// special form for A
-	double b[] = {
-		1, 2, 3, 0, 0,
-		4, 5, 6, 0, 0,
-		7, 8, 9, 0, 0,
-		0, 0, 0, 1, 0,
-		0, 0, 0, 0, 1
+	Polynomial b[] = {
+		T(1), T(2), T(3), T(0), T(0),
+		T(4), T(5), T(6), T(0), T(0),
+		T(7), T(8), T(9), T(0), T(0),
+		T(0), T(0), T(0), T(1), T(0),
+		T(0), T(0), T(0), T(0), T(1)
 	};
 
 	B = Matrix(5, 5, b);
@@ -403,12 +410,12 @@ TEST_F(MatrixMultiplication, bmmCaABbC)
 TEST_F(MatrixMultiplication, mmCasABbC)
 {
    	// special form for A
-	double a[] = {
-		0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0,
-		1, 2, 3, 4, 5,
-		5, 4, 3, 2, 1
+	Polynomial a[] = {
+		T(0), T(0), T(0), T(0), T(0),
+		T(0), T(0), T(0), T(0), T(0),
+		T(0), T(0), T(0), T(0), T(0),
+		T(1), T(2), T(3), T(4), T(5),
+		T(5), T(4), T(3), T(2), T(1)
 	};
 	A = Matrix(5, 5, a);
 
@@ -420,12 +427,12 @@ TEST_F(MatrixMultiplication, mmCasABbC)
 TEST_F(MatrixMultiplication, mmCaAsBbC)
 {
    	// special form for B
-	double b[] = {
-		0, 0, 0, 1, 5,
-		0, 0, 0, 2, 4,
-		0, 0, 0, 3, 3,
-		0, 0, 0, 4, 2,
-		0, 0, 0, 5, 1
+	Polynomial b[] = {
+		T(0), T(0), T(0), T(1), T(5),
+		T(0), T(0), T(0), T(2), T(4),
+		T(0), T(0), T(0), T(3), T(3),
+		T(0), T(0), T(0), T(4), T(2),
+		T(0), T(0), T(0), T(5), T(1)
 	};
 	B = Matrix(5, 5, b);
 
@@ -436,22 +443,22 @@ TEST_F(MatrixMultiplication, mmCaAsBbC)
 
 TEST_F(MatrixMultiplication, mmCaAUTBPbC)
 {
-	double bUT[] = {
-		1, 2, 3, 4, 5,
-		0, 6, 7, 8, 9,
-		0, 0, 1, 2, 3,
-		0, 0, 0, 4, 5,
-		0, 0, 0, 0, 6
+	Polynomial bUT[] = {
+		T(1), T(2), T(3), T(4), T(5),
+		T(0), T(6), T(7), T(8), T(9),
+		T(0), T(0), T(1), T(2), T(3),
+		T(0), T(0), T(0), T(4), T(5),
+		T(0), T(0), T(0), T(0), T(6)
 	};
 	Matrix B_UT = Matrix(5, 5, bUT);
 	
 	// special form for B
-	double b[] = {
-		1, 3, 2, 4, 5,
-		0, 7, 6, 8, 9,
-		0, 1, 0, 2, 3,
-		0, 0, 0, 4, 5,
-		0, 0, 0, 0, 6
+	Polynomial b[] = {
+		T(1), T(3), T(2), T(4), T(5),
+		T(0), T(7), T(6), T(8), T(9),
+		T(0), T(1), T(0), T(2), T(3),
+		T(0), T(0), T(0), T(4), T(5),
+		T(0), T(0), T(0), T(0), T(6)
 	};
 	B = Matrix(5, 5, b);
 	int piv[] = { 0, 2, 1, 3, 4 } ;
@@ -487,28 +494,28 @@ TEST_F(MatrixMultiplication, mmCaATBbC)
 
 TEST_F(MatrixMultiplication, mmCaATBPbC)
 {
-	double a1[] = {
-		1, 2, 3, 4, 5,
-		6, 7, 8, 9, 1,
-		0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0
+	Polynomial a1[] = {
+		T(1), T(2), T(3), T(4), T(5),
+		T(6), T(7), T(8), T(9), T(1),
+		T(0), T(0), T(0), T(0), T(0),
+		T(0), T(0), T(0), T(0), T(0),
+		T(0), T(0), T(0), T(0), T(0)
 	};
 	
-	double b1[] = {
-		1, 2, 3, 4, 5,
-		6, 7, 8, 9, 1,
-		0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0
+	Polynomial b1[] = {
+		T(1), T(2), T(3), T(4), T(5),
+		T(6), T(7), T(8), T(9), T(1),
+		T(0), T(0), T(0), T(0), T(0),
+		T(0), T(0), T(0), T(0), T(0),
+		T(0), T(0), T(0), T(0), T(0)
 	};
 	
-	double b2[] = {
-		1, 2, 3, 5, 4,
-		6, 7, 8, 1, 9,
-		0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0
+	Polynomial b2[] = {
+		T(1), T(2), T(3), T(5), T(4),
+		T(6), T(7), T(8), T(1), T(9),
+		T(0), T(0), T(0), T(0), T(0),
+		T(0), T(0), T(0), T(0), T(0),
+		T(0), T(0), T(0), T(0), T(0)
 	};
 
 	A = Matrix(5, 5, a1);
@@ -534,15 +541,15 @@ TEST_F(MatrixMultiplication, mmCaABTbC1)
 
 TEST_F(MatrixMultiplication, mmCaABTbC_up)
 {
-	double b[] = {
-		1, 6, 0, 0, 0,
-		2, 7, 0, 0, 0,
-		3, 8, 0, 0, 0,
-		4, 9, 0, 0, 0,
-		5, 1, 0, 0, 0
+	Polynomial b[] = {
+		T(1), T(6), T(0), T(0), T(0),
+		T(2), T(7), T(0), T(0), T(0),
+		T(3), T(8), T(0), T(0), T(0),
+		T(4), T(9), T(0), T(0), T(0),
+		T(5), T(1), T(0), T(0), T(0)
 	};
 
-	B = Matrix(5,5, b);
+	B = Matrix(5, 5, b);
 	Matrix bt = B.asTranspose();
     Matrix expect = (A * bt * alpha) + (C * beta);
 
@@ -552,15 +559,15 @@ TEST_F(MatrixMultiplication, mmCaABTbC_up)
 
 TEST_F(MatrixMultiplication, mmCaABTbC_down)
 {
-	double b[] = {
-		0, 0, 0, 1, 6,
-		0, 0, 0, 2, 7,
-		0, 0, 0, 3, 8,
-		0, 0, 0, 4, 9,
-		0, 0, 0, 5, 1
+	Polynomial b[] = {
+		T(0), T(0), T(0), T(1), T(6),
+		T(0), T(0), T(0), T(2), T(7),
+		T(0), T(0), T(0), T(3), T(8),
+		T(0), T(0), T(0), T(4), T(9),
+		T(0), T(0), T(0), T(5), T(1)
 	};
 
-	B = Matrix(5,5, b);
+	B = Matrix(5, 5, b);
 	Matrix bt = B.asTranspose();
     Matrix expect = (A * bt * alpha) + (C * beta);
 
@@ -570,13 +577,13 @@ TEST_F(MatrixMultiplication, mmCaABTbC_down)
 
 TEST_F(MatrixMultiplication, bmmCaABTbC)
 {
-	// case 1
-	double a[] = {
-		1, 2, 3, 0, 0,
-		4, 5, 6, 0, 0,
-		7, 8, 9, 0, 0,
-		0, 0, 0, 1, 0,
-		0, 0, 0, 0, 1
+	// case T(1)
+	Polynomial a[] = {
+		T(1), T(2), T(3), T(0), T(0),
+		T(4), T(5), T(6), T(0), T(0),
+		T(7), T(8), T(9), T(0), T(0),
+		T(0), T(0), T(0), T(1), T(0),
+		T(0), T(0), T(0), T(0), T(1)
 	};
 	A = Matrix(5, 5, a);
 	
@@ -590,17 +597,17 @@ TEST_F(MatrixMultiplication, bmmCaABTbC)
 	tempC.bmmCaABTbC(2, 2, alpha, beta, A, B);
 	ASSERT_NE(expect, tempC);
 
-	// case 2
-	double a2[] = {
-		1,2,3,0,
-		4,5,6,0,
-		0,0,0,1
+	// case T(2)
+	Polynomial a2[] = {
+		T(1), T(2), T(3), T(0),
+		T(4), T(5), T(6), T(0),
+		T(0), T(0), T(0), T(1)
 	};
-	double b2[] = {
-		1,2,3,
-		4,5,6,
-		0,0,0,
-		0,0,0
+	Polynomial b2[] = {
+		T(1), T(2), T(3),
+		T(4), T(5), T(6),
+		T(0), T(0), T(0),
+		T(0), T(0), T(0)
 	};
 
 	A = Matrix(3, 4, a2);
@@ -639,10 +646,10 @@ TEST_F(MatrixMultiplication, mmCaAIbC)
 TEST_F(MatrixMethods, cpermutem)
 {
 	int newOrder[] = {3, 0, 2, 1};
-	double b[] = {
-		 4,  1,  3,  2,
-		 8,  5,  7,  6,
-		12,  9,  11, 10
+	Polynomial b[] = {
+		 T(4),  T(1),  T(3),  T(2),
+		 T(8),  T(5),  T(7),  T(6),
+		T(12),  T(9),  T(11), T(10)
 	};
 	Matrix newB = Matrix(3, 4, b);
 
@@ -655,10 +662,10 @@ TEST_F(MatrixMethods, cpermutem_trans)
 	int newOrder[] = {1, 3, 2, 0};
 	// Beim Transpose wird aus dem Pivot Vektor -> { 3, 0, 2, 1 }
 
-	double b[] = {
-		 4,  1,  3,  2,
-		 8,  5,  7,  6,
-		12,  9,  11, 10
+	Polynomial b[] = {
+		 T(4),  T(1),  T(3),  T(2),
+		 T(8),  T(5),  T(7),  T(6),
+		T(12),  T(9),  T(11), T(10)
 	};
 	Matrix newB = Matrix(3, 4, b);
 
@@ -669,10 +676,10 @@ TEST_F(MatrixMethods, cpermutem_trans)
 TEST_F(MatrixMethods, rpermutem)
 {
 	int newOrder[] = {1, 2, 0};
-	double b[] = {
-		5,  6,  7,  8,
-		9, 10, 11, 12,
-		1,  2,  3,  4
+	Polynomial b[] = {
+		T(5),  T(6),  T(7),  T(8),
+		T(9), T(10), T(11), T(12),
+		T(1),  T(2),  T(3),  T(4)
 	};
 	Matrix newB = Matrix(3, 4, b);
 
@@ -683,21 +690,21 @@ TEST_F(MatrixMethods, rpermutem)
 TEST_F(MatrixMethods, transpose)
 {
 	// test in place transpose
-	double atrans[] = {
-		1, 4, 7,
-		2, 5, 8,
-		3, 6, 9
+	Polynomial atrans[] = {
+		T(1), T(4), T(7),
+		T(2), T(5), T(8),
+		T(3), T(6), T(9)
 	};
 	Matrix Atranspose(3, 3, atrans);
 	A.transpose();
 	ASSERT_EQ(Atranspose, A);
 
 	// test normal transpose
-	double btrans[] = {
-		1,  5,  9,
-		2,  6, 10,
-		3,  7, 11,
-		4,  8, 12
+	Polynomial btrans[] = {
+		T(1),  T(5),  T(9),
+		T(2),  T(6), T(10),
+		T(3),  T(7), T(11),
+		T(4),  T(8), T(12)
 	};
 	Matrix Btranspose(4, 3, btrans);
 	B.transpose();
@@ -706,19 +713,19 @@ TEST_F(MatrixMethods, transpose)
 
 TEST_F(MatrixMethods, asTranspose)
 {
-	double atrans[] = {
-		1, 4, 7,
-		2, 5, 8,
-		3, 6, 9
+	Polynomial atrans[] = {
+		T(1), T(4), T(7),
+		T(2), T(5), T(8),
+		T(3), T(6), T(9)
 	};
 	Matrix Atranspose(3, 3, atrans);
 	ASSERT_EQ(Atranspose, A.asTranspose());
 
-	double btrans[] = {
-		1,  5,  9,
-		2,  6, 10,
-		3,  7, 11,
-		4,  8, 12
+	Polynomial btrans[] = {
+		T(1),  T(5),  T(9),
+		T(2),  T(6), T(10),
+		T(3),  T(7), T(11),
+		T(4),  T(8), T(12)
 	};
 	Matrix Btranspose(4, 3, btrans);
 	ASSERT_EQ(Btranspose, B.asTranspose());
@@ -771,10 +778,10 @@ TEST_F(MatrixMethods, set2Zero)
 	A.set2Zero();
 	ASSERT_EQ(Z, A);
 
-	double zeros[] = {
-		0, 0, 0, 0,
-		0, 0, 0, 0,
-		0, 0, 0, 0
+	Polynomial zeros[] = {
+		T(0), T(0), T(0), T(0),
+		T(0), T(0), T(0), T(0),
+		T(0), T(0), T(0), T(0)
 	};
 	Matrix Zeros(3, 4, zeros);
 	
@@ -796,10 +803,10 @@ TEST_F(MatrixMethods, set2ZeroFromIndices)
 
 TEST_F(MatrixMethods, set2Val)
 {
-	double b42[] = {
-		42, 42, 42, 42,
-		42, 42, 42, 42,
-		42, 42, 42, 42,
+	Polynomial b42[] = {
+		T(42), T(42), T(42), T(42),
+		T(42), T(42), T(42), T(42),
+		T(42), T(42), T(42), T(42),
 	};
 	Matrix B42(3, 4, b42);
 
@@ -809,13 +816,13 @@ TEST_F(MatrixMethods, set2Val)
 
 TEST_F(MatrixMethods, set2Val2)
 {
-	C.set2Val(2, 3, 1, 2, 3.0);
+	// C.set2Val(2, 3, T(1), T(2), T(3).0);
 	ASSERT_EQ(C3, C);
 }
 
 TEST_F(MatrixMethods, set2ValFromIndices)
 {
-	C.set2ValFromIndices(2, 4, 1, 4, 3.0);
+	// C.set2ValFromIndices(2, 4, T(1), T(4), T(3).0);
 	ASSERT_EQ(C3, C);
 }
 
@@ -840,21 +847,21 @@ TEST_F(MatrixExceptions, FunctionExceptions)
 {
 	// mmCaABbC
 	// Wrong Dimension of receiving Matrix
-	Matrix C(3,3);
+	Matrix C(3, 3);
 	ASSERT_THROW(C.mmCaABbC(1, 1, A, B),MathException);
-	C = Matrix(3,2);
+	C = Matrix(3, 2);
 	C.mmCaABbC(1, 1, A, B); // should be okay
 	// Wrong Multiplication Dimensions
 	ASSERT_THROW(C.mmCaABbC(1, 1, B, A),MathException);
 
 	// bmmCaABbC
 	// rows > rows of B
-	ASSERT_THROW(C.bmmCaABbC(5,2,1,1,A,B),MathException);
+	ASSERT_THROW(C.bmmCaABbC(5, 2, 1, 1, A, B), MathException);
 	// cols > cols of B
-	ASSERT_THROW(C.bmmCaABbC(2,5,1,1,A,B),MathException);
+	ASSERT_THROW(C.bmmCaABbC(2, 5, 1, 1, A, B), MathException);
 	// A.cols != B.rows
-	ASSERT_THROW(C.bmmCaABbC(1,1,1,1,B,A),MathException);
-	C = Matrix(1,2);
+	ASSERT_THROW(C.bmmCaABbC(1, 1, 1, 1, B, A), MathException);
+	C = Matrix(1, 2);
 	// A.rows != C.rows || A.cols != C.cols
-	ASSERT_THROW(C.bmmCaABbC(1,1,1,1,A,B),MathException);
+	ASSERT_THROW(C.bmmCaABbC(1, 1, 1, 1, A, B), MathException);
 }
